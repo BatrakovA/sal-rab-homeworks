df8ec74d53f65e330aa4cac832187a2a3dfe4c38
BatrakovA <mr_asd@mail.ru>
2022-06-01 13:26:48 +0300

    Задание 1

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,6 +5,7 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message = '';
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'за'
@@ -12,8 +13,9 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message значение переменной productPrice
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = productName + ' за ' + productPrice + ' теперь в корзине!'
     // выведите переменную message в консоль
-
+    console.log(message);
     // Конец решения задания №1.1.
 
     return message;
@@ -23,11 +25,11 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
     // создайте переменную newValue
     // присвойте ей значение oldValue
+    let newValue = oldValue;
     // увеличьте значение newValue на 1
-
+    newValue += 1;
     // Конец решения задания №1.2.
 
     return newValue;
@@ -40,9 +42,11 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    let newSum = oldSum + difference;
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    let newSumText = String(newSum) + ' ₽'; // какое такое склеивание? конкатенация
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    newSumText = `${newSum} ₽`;
     // Конец решения задания №1.3.
 
     return newSumText;
